ffb0652aa038003cb3be8e40c6a9fa3444009660
Roxana <roxanarevilla@outlook.de>
2023-05-22 16:53:11 +0200

    /* Instanziierung Klasse --> Objekt */      // Cat -- Referenztyp      // cat -- Referenzvariable

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,8 +1,42 @@
 package com.cc.java;
 
+
 public class App {
     
-    
+    public static void main(String[] args) {
+        
+     /* Instanziierung Klasse --> Objekt */
+     // Cat -- Referenztyp
+     // cat -- Referenzvariable  
+
+       Cat cat1 = new Cat();
+        output("Blick von aussen: " + cat1);
+        output(cat1.telYourAddress());
+
+        cat1.telYourAddress();
+        
+
+
+        // output(cat1.telYourAddress);
+       ;
+
+        output("--------");
+
+    //    System.out.println("----------");
+
+        Cat cat2 = new Cat();
+        output(cat2.telYourAddress());
+        // System.out.println("Blick von aussen: "  + cat2);
+
+        cat2.telYourAddress();
+
+
+    }   
+
+    public static void output(String outStr) {
+        System.out.println(outStr);
+        
+    }
 
 }
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,16 @@
+package com.cc.java;
+
+public class Cat {
+
+ // this ---> Instanzvariable //Selbstrefenz
+ public String telYourAddress() {
+    return("Blick von innen" + this);
+    
+    // System.out.println("Blick von innen: " + this);  
+
+ }   
+
+
+
+
+}
