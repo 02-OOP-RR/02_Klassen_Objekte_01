310b485734c7d9080fa0333b2359c9e2e01c2f1a
Roxana <roxanarevilla@outlook.de>
2023-05-24 09:38:49 +0200

    Getter und Setter

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -9,31 +9,72 @@ public class App {
      // Cat -- Referenztyp
      // cat -- Referenzvariable  
 
-       Cat cat1 = new Cat();
-        output("Blick von aussen: " + cat1);
-        output(cat1.telYourAddress());
+       Cat cat1 = new Cat("Grizabella", "white", 29);
+
+    //    cat1.furColor =  "white";
+
+
+        // output("Blick von aussen: " + cat1);
+        // output(cat1.telYourAddress());
+
+    // Wertzuweisung    
+        // cat1.name = "Grizabella"; 
+        // cat1.furColor = "white"; 
+        // cat1.age = 29; 
+
+
+
+        // Ausgabe 
+        // output(cat1.name);
+        // output(cat1.furColor);
+        // output(Integer.toString(cat1.age)   );
+
+        output(cat1.getName());
+        output(cat1.getFurColor());
+        output(Integer.toString(cat1.getAge()) );
+
+
+
 
         cat1.telYourAddress();
-        
 
+    
 
         // output(cat1.telYourAddress);
-       ;
-
+    
         output("--------");
 
-    //    System.out.println("----------");
+    //System.out.println("----------");
 
-        Cat cat2 = new Cat();
-        output(cat2.telYourAddress());
-        // System.out.println("Blick von aussen: "  + cat2);
+        // Cat cat2 = new Cat();
 
+        // // Wertzuweisung    
+        // cat2.name = "Alonso"; 
+        // cat2.furColor = "grey"; 
+        // cat2.age = 35; 
+
+        Cat cat2 = new Cat("Alonso", "grey", 35);
+        // cat2 = new Cat("Alonso", "grey", 35);
+        // cat2.name = "Alonso"; 
+
+
+
+        //Ausgabe direkt
+        output(cat2.getName());
+        output(cat2.getFurColor());
+        output(Integer.toString(cat2.getAge())   );
+
+       
+
+
+        
         cat2.telYourAddress();
+        // System.out.println("Blick von aussen: "  + cat2);
 
 
     }   
 
-    public static void output(String outStr) {
+    private static void output(String outStr) {
         System.out.println(outStr);
         
     }
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,8 +1,76 @@
 package com.cc.java;
 
+
 public class Cat {
 
- // this ---> Instanzvariable //Selbstrefenz
+private String name;
+private String furColor;
+private int age;
+
+
+public Cat(String name, String furColor, int age) {
+   this.name = name;
+   this.furColor = furColor;
+   this.age = age;
+}
+
+
+
+
+
+// public String tellYourName() {
+//   return this. name;
+// }
+// public String tellfurColor() {
+//    return this.furColor;
+
+// }
+// public int tellYourAge() {
+//    return this.age;
+   
+// }
+
+
+// Getter
+ public String getName() {
+   if (this.checkPermission()) {
+      return name;
+
+   } else {
+      return "Sorry"; 
+      
+   }
+}
+
+private boolean checkPermission() {
+   return false;
+}
+
+
+public String getFurColor() {
+   return furColor;
+}
+
+//Setter
+public void setFurColor(String furColor) {
+   this.furColor = furColor;
+}
+
+// Age
+public int getAge() {
+   return age ;
+
+}
+
+public void setAge(int age) {
+   this.age = age;
+}
+
+
+
+
+
+// this ---> Instanzvariable //Selbstrefenz
  public String telYourAddress() {
     return("Blick von innen" + this);
     
